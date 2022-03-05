7e628e2492700f36e8b207e8616ff37254de99d7
AhmadAmer7 <aamr76090@gmail.com>
2022-03-05 23:42:03 +0200

    ForgetPassword Feature

---
+++ config/googleData.js
@@ -0,0 +1,3 @@
+module.exports.clientId ="257152859647-fvb220i5ifu4alekjc7al8tkfioq095l.apps.googleusercontent.com";
+module.exports.clientSecret = "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";
+module.exports.callBackURL = "http://localhost:8000/google/callback";
